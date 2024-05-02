8710382c59f487a17393d93a3b18e73e82c3d2f0
BelalMuhammad00 <belalhamo123@gmail.com>
2024-05-02 21:55:46 +0300

    modify particles

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
 	
 
 	/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', '/js/particles.json', function() {
+particlesJS.load('particles-js', 'particles.json', function() {
 	console.log('callback - particles.js config loaded');
   });
 
