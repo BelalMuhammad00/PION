116c16ed6bae6d1f99918fc9f601660ccb89c8da
BelalMuhammad00 <belalhamo123@gmail.com>
2024-05-03 16:14:56 +0300

    modify some css

--- js/main.js
+++ js/main.js
@@ -195,20 +195,20 @@ particlesJS.load('particles-js', 'particles.json', function() {
   	/*----------------------------------------------------*/
   	/* Smooth Scrolling
   	------------------------------------------------------ */
-  	$('.smoothscroll').on('click', function (e) {
+  	// $('.smoothscroll').on('click', function (e) {
 	 	
-	 	e.preventDefault();
+	//  	e.preventDefault();
 
-   	var target = this.hash,
-    	$target = $(target);
+   	// var target = this.hash,
+    // 	$target = $(target);
 
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
+    // 	$('html, body').stop().animate({
+    //    	'scrollTop': $target.offset().top
+    //   }, 800, 'swing', function () {
+    //   	window.location.hash = target;
+    //   });
 
-  	});  
+  	// });  
   
 
    /*----------------------------------------------------*/
